5d4005084fa75fdf5372df5e5bd31d678bed47ea
PCM28 <104649488+PCM28@users.noreply.github.com>
2022-11-29 14:29:47 +0100

    borrare

--- Back-end/Flights/src/main/java/com/example/Flights/repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Flights.repository;
-
-import com.example.Flights.model.Flight;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightRepository extends MongoRepository<Flight, String> {
-}
